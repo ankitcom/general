b940923ad211441401f82a33bb1defac85287de0
Ankit Bhatnagar <ankit.bhatnagar@timesinternet.in>
2017-08-19 15:50:23 +0530

    Reformat Numbers updated answer

--- Questions/src/com/ques/ReformatNumber.java
+++ Questions/src/com/ques/ReformatNumber.java
@@ -3,30 +3,60 @@ package com.ques;
 public class ReformatNumber {
 
 	public static void main(String[] args) {
-		System.out.println(new ReformatNumber().solution("0000000000"));
+		System.out.println(new ReformatNumber().solution("- 123456789-345-0222 07-0--  "));
 	}
 	
 	public String solution(String S) {
-		S=S.replace("-", "").replace(" ", "");
-		int length=S.length();
-		StringBuffer sb=new StringBuffer(S);
+		StringBuffer cleanNumbers=cleanNumbers(S);
 		
-		int itrLen=length;
+		int length=cleanNumbers.length();
+		
+		int itLen=length;
 		if(length%3==2&&length>2){
-			sb.replace(length-2, length-2, "-");
-			itrLen=length-2;
+			itLen=length-2;
+			cleanNumbers.replace(length-2, length-2, "-");
+			length++;
 		}else if(length%3==1){
-			sb.replace(length-2, length-2, "-");
-			if(length>4) sb.replace(length-4, length-4, "-");
-			itrLen=length-4;
-		}
-		for(int i=3;i<itrLen;i=i+3){
-			sb.replace(i, i, "-");
-			//to accomodate the added character
-			i++;
-			itrLen++;
+			itLen=length-4;
+			cleanNumbers.replace(length-2, length-2, "-");
+			if(length>4){
+				cleanNumbers.replace(length-4, length-4, "-");
+				length++;
+			}
+			length++;
 		}
 		
-		return sb.toString();
+		int resultLength=length;
+		if(itLen>3){
+			resultLength=length+(itLen/3-1);
+			char[] result=new char[resultLength];
+			int i=0,j=0;
+			for(;i<resultLength-(length-itLen+3);i++){
+				if(i%4==3){
+					result[i]='-';
+				}else{
+					result[i]=cleanNumbers.charAt(j);
+					j++;
+				}
+			}
+			for(;i<resultLength;i++,j++){
+				result[i]=cleanNumbers.charAt(j);
+			}
+			return String.valueOf(result);
+		}else{
+			return cleanNumbers.toString();
+		}
+		
     }
+
+	private StringBuffer cleanNumbers(String S) {
+		StringBuffer cleanNumbers=new StringBuffer();
+
+		for(int i=0;i<S.length();i++){
+			if(S.charAt(i)!='-' && S.charAt(i)!=' '){
+				cleanNumbers.append(S.charAt(i));
+			}
+		}
+		return cleanNumbers;
+	}
 }
