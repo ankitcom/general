6249008e4cbd16a78af44924d38bf0744ac0bbb4
Ankit Bhatnagar <ankbhatn@adobe.com>
2020-06-30 20:49:51 +0530

    deleting old unused files

--- CrossoverLog/src/main/java/com/crossover/data/DataAccess.java
+++
@@ -1,9 +0,0 @@
-package com.crossover.data;
-
-import java.util.List;
-
-public interface DataAccess {
-
-	public void storeLog(String userId, String userLog) throws Exception;
-	public List<String> getUserLogs() throws Exception;
-}
